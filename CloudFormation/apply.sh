78d0f4779c524aab738b315550af1cf929f34c4f
Diego Satizabal <diego.satizabal@gmail.com>
2025-04-08 10:12:39 -0300

    08Apr2025

--- CloudFormation/apply.sh
+++ CloudFormation/apply.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Deploy to multiple regions with region-specific CIDRs
-REGIONS=("us-east-1" "us-west-2" "eu-central-1")
+# aws cloudformation describe-stack-events --stack-name MultiRegionVPC-us-east-1
+# REGIONS=("us-east-1" "us-west-2" "eu-central-1")
+REGIONS=("us-east-1")
 
 for i in "${!REGIONS[@]}"; do
   region=${REGIONS[$i]}
@@ -10,5 +12,6 @@ for i in "${!REGIONS[@]}"; do
     --template-file region.yaml \
     --stack-name "MultiRegionVPC-$region" \
     --parameter-overrides \
-        Region=$region
+        Region=$region \
+    --capabilities CAPABILITY_AUTO_EXPAND
 done
--- CloudFormation/destroy.sh
+++ CloudFormation/destroy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Deploy to multiple regions with region-specific CIDRs
 REGIONS=("us-east-1" "us-west-2" "eu-central-1")
+# REGIONS=("us-east-1")
 
 for i in "${!REGIONS[@]}"; do
   region=${REGIONS[$i]}
